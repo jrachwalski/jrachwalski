0cd8dbc3f00e859bd5b3288db2990b9b40c7f039
Jared Rachwalski <jaredr@shaw.ca>
2021-11-01 09:17:59 -0700

    Password generator script

--- Password-generator.ps1
+++ Password-generator.ps1
@@ -1,4 +1,10 @@
-#Random password generator
+# Random password generator
+#
+write-host "Create N number of passwords,
+each will have a user-specified number of lowercase characters, 
+plus 1 Uppercase letter, 
+1 number and 1 special character."
+
 
 function Get-RandomCharacters($length, $characters) {
     $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
@@ -6,22 +12,27 @@ function Get-RandomCharacters($length, $characters) {
     return [String]$characters[$random]
 }
 
+<#
 function Scramble-String([string]$inputString){     
     $characterArray = $inputString.ToCharArray()   
     $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
     $outputString = -join $scrambledStringArray
     return $outputString 
 }
-#amount of passwords to generate:
-$num = 21
+#>
+
+# Prompt user for amount of passwords to generate and length:
+$num = read-host "How many Passwords to generate?"
+$len = read-host "How many characters should the passwords be?"
 
 
 do {
 #change the "length" to get longer passwords
-$password = Get-RandomCharacters -length 7 -characters 'abcdefghiklmnoprstuvwxyz'
+$password = Get-RandomCharacters -length ($len - 3) -characters 'abcdefghiklmnoprstuvwxyz'
 $password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
-$password += Get-RandomCharacters -length 2 -characters '1234567890'
-$password += Get-RandomCharacters -length 2 -characters '!"$%&/()=?}][{@#*+'
-$num--
+$password += Get-RandomCharacters -length 1 -characters '1234567890'
+$password += Get-RandomCharacters -length 1 -characters '!"$%&/()=?}][{@#*+'
+#$password = scramble-string
+$num -= 1
 Write-Host $password
 } while ($num -gt 0)
