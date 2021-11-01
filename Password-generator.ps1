eb8a810489cd616a29c08681cbf716be7b18a2dd
jrachwalski <34454276+jrachwalski@users.noreply.github.com>
2021-11-01 08:51:00 -0700

    Add files via upload

---
+++ Password-generator.ps1
@@ -0,0 +1,27 @@
+#Random password generator
+
+function Get-RandomCharacters($length, $characters) {
+    $random = 1..$length | ForEach-Object { Get-Random -Maximum $characters.length }
+    $private:ofs=""
+    return [String]$characters[$random]
+}
+
+function Scramble-String([string]$inputString){     
+    $characterArray = $inputString.ToCharArray()   
+    $scrambledStringArray = $characterArray | Get-Random -Count $characterArray.Length     
+    $outputString = -join $scrambledStringArray
+    return $outputString 
+}
+#amount of passwords to generate:
+$num = 21
+
+
+do {
+#change the "length" to get longer passwords
+$password = Get-RandomCharacters -length 7 -characters 'abcdefghiklmnoprstuvwxyz'
+$password += Get-RandomCharacters -length 1 -characters 'ABCDEFGHKLMNOPRSTUVWXYZ'
+$password += Get-RandomCharacters -length 2 -characters '1234567890'
+$password += Get-RandomCharacters -length 2 -characters '!"$%&/()=?}][{@#*+'
+$num--
+Write-Host $password
+} while ($num -gt 0)
